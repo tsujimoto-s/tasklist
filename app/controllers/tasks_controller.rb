728f96468ee5ea218f7be850af3f545eccd38e90
Akihiro Shinozawa <ackeyzawa.zawa@gmail.com>
2017-04-21 05:33:52 +0000

    bootstrap ver

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,6 @@ class TasksController < ApplicationController
   end
 
   def show
-    set_task
   end
 
   def new
@@ -26,11 +25,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    set_task
   end
 
   def update
-    set_task
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
@@ -41,11 +38,14 @@ class TasksController < ApplicationController
   end
 
   def destroy
-     set_task
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
+  end
+  
+  def index
+    @tasks = Task.all.page(params[:page])
   end
   
   private
